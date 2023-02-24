2a98941d1b0b905adaa24f77f458224619e2a4d4
NguyenMinhLuanSTU <95679153+NguyenMinhLuanSTU@users.noreply.github.com>
2023-02-24 16:43:05 +0700

    Add files via upload
    css html js

---
+++ wedding website/script.js
@@ -0,0 +1,8 @@
+let items = document.querySelectorAll('.item');
+document.addEventListener('scroll', (event)=>{
+    items.forEach(item=>{
+        if(item.offsetTop - window.scrollY < 350){
+            item.classList.add('active');
+        }
+    })
+})
